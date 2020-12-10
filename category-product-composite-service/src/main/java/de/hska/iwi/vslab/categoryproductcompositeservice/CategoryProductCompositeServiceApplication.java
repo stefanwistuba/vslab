41dd818e7750d7db99bc100530eb65edfb7173ed
Minh Le <minh.le1995@gmail.com>
2020-12-10 08:18:23 +0100

    Fixing tries

--- category-product-composite-service/src/main/java/de/hska/iwi/vslab/categoryproductcompositeservice/CategoryProductCompositeServiceApplication.java
+++ category-product-composite-service/src/main/java/de/hska/iwi/vslab/categoryproductcompositeservice/CategoryProductCompositeServiceApplication.java
@@ -3,6 +3,8 @@ package de.hska.iwi.vslab.categoryproductcompositeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
@@ -11,10 +13,11 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrixDashboard
 @RibbonClient("category-product-composite-service")
 public class CategoryProductCompositeServiceApplication {
+	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
--- zuul/src/main/java/de/hska/iwi/vslab/zuul/ZuulApplication.java
+++ zuul/src/main/java/de/hska/iwi/vslab/zuul/ZuulApplication.java
@@ -2,12 +2,13 @@ package de.hska.iwi.vslab.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {
