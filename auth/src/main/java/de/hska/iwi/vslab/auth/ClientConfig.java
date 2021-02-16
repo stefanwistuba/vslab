889714d60e73063cbdcaacfcf1fab817d8e95b51
Stefan Wistuba <stefan.wistuba@outlook.de>
2021-02-16 22:18:02 +0100

    .

---
+++ auth/src/main/java/de/hska/iwi/vslab/auth/ClientConfig.java
@@ -0,0 +1,40 @@
+package de.hska.iwi.vslab.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@Configuration
+@EnableOAuth2Client
+public class ClientConfig {
+
+	@Bean
+	@LoadBalanced
+	public OAuth2RestTemplate restTemplate(
+			@Qualifier("authServerClientCredentialsResourceDetails") OAuth2ProtectedResourceDetails resourceDetails) {
+		return new OAuth2RestTemplate(resourceDetails);
+	}
+
+	@ConfigurationProperties(prefix = "security.oauth2.client.auth-server-client-credentials")
+	@Bean
+	public OAuth2ProtectedResourceDetails authServerClientCredentialsResourceDetails() {
+		return new ClientCredentialsResourceDetails();
+	}
+
+}
