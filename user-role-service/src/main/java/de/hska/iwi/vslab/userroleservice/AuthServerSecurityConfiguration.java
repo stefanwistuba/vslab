7b8b36c4f669d2e9c0b558581c353aac96f0bbb4
Stefan Wistuba <stefan.wistuba@outlook.de>
2021-02-16 22:33:15 +0100

    ...

--- user-role-service/src/main/java/de/hska/iwi/vslab/userroleservice/AuthServerSecurityConfiguration.java
+++ user-role-service/src/main/java/de/hska/iwi/vslab/userroleservice/AuthServerSecurityConfiguration.java
@@ -1,39 +1,20 @@
 package de.hska.iwi.vslab.userroleservice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.http.HttpMethod;
-import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class AuthServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class ResourceSecurityConfig extends ResourceServerConfigurerAdapter {
 
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.anyRequest().authenticated();
+	}
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated();
-    }
 }
