beb3ba3b7a012fdd953c2c66a0fb081a42283957
Stefan Wistuba <stefan.wistuba@outlook.de>
2021-02-16 22:59:15 +0100

    .

--- user-role-service/src/main/java/de/hska/iwi/vslab/userroleservice/ResourceSecurityConfig.java
+++ user-role-service/src/main/java/de/hska/iwi/vslab/userroleservice/ResourceSecurityConfig.java
@@ -13,8 +13,7 @@ public class ResourceSecurityConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.anyRequest().authenticated();
+		http.authorizeRequests().anyRequest().authenticated();
 	}
 
 }
